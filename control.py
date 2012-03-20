763f8b63fca343a4878317a688607e6c737180a6
Christian Fobel <christian@fobel.net>
2012-03-20 15:47:25 -0400

    Refactor control into CoalitionControl class
    The idea here is to allow easy control of a coalition server from Python by
    providing add, remove, and list actions.
    Modify control.py script to use argparse and CoalitionControl class.

--- control.py
+++ control.py
@@ -1,141 +1,144 @@
-import sys, getopt, urllib, httplib, re
+import sys
+import getopt
+import urllib
+import httplib
+import re
+from copy import deepcopy
 
-global cmd, serverUrl, dir, title, action,id
-dir = "."
-title = "New job"
-startIndex = 1
-endIndex = 1
-retry = 10
-affinity = ""
-priority = 1000
-timeout = 0
-parent = 0
-dependencies = ""
-id=-1
-localprogress=None
-globalprogress=None
 
-def usage():
-	print ("Usage: control.py [OPTIONS] SERVER_URL ACTION [COMMAND]")
-	print ("Control the Coalition server located at SERVER_URL.\n")
-	print("Actions:")
-	print("  add: add a job, use option -c for command")
-	print("  list: list the jobs on the server")
-	print("  remove: remove job designated by id, option -i is necessary") 
-	print ("Options:")
-	print ("  -h, --help\t\tShow this help")
-	print ("  -v, --verbose\t\tIncrease verbosity")
-	print ("  -c, --cmd=COMMAND\t\tIf action is add, add command to server")
-	print ("  -d, --directory=DIR\tWorking directory (default: "+dir+")")
-	print ("  -t, --title=TITLE\tSet the job title (default: "+title+")")
-	print ("  -p, --priority=PRIORITY\tPriority of the job (default: "+str(priority)+")")
-	print ("  -r, --retry=RETRY\tNumber of retry this jobs can do (default: "+str(retry)+")")
-	print ("  -a, --affinity=AFFINITY\tAffinity words to workers, separated by a comma (default: \"\"")
-	print ("  -i, --jobid=JOBID\tID of the Job")
-	print ("  -T, --timeout=TIMEOUT\ttimeout for the job")
-	print ("  -D, --dependencies=DEPS\tIDs of the dependent jobs (exemple : \"21 22 23\"")
-	print ("  -P, --parent=PARENT\tId of of the parent of the job")
-	print ("      --globalprogress=PATTERN\tThe job progression pattern")
-	print ("      --localprogress=PATTERN\tThe second job progression pattern")
+class CoalitionControl(object):
+    default_options = dict(dir='.', title='New job', startIndex=1, endIndex=1,
+            retry=10, affinity='', priority=1000, timeout=0, parent=0,
+            dependencies='', id=-1, localprogress=None, globalprogress=None,)
 
-	print ("\nExample : control -t \"Job\" -a \"Linux\" -c \"echo Hello world!\" http://localhost:8080 add")
+    def __init__(self, server_url):
+        clean_url = re.sub(r'^http://', '', server_url)
+        clean_url = re.sub(r'/*$', '', clean_url)
+        self.server_url = clean_url
 
-# Parse the options
-try:
-	opts, args = getopt.getopt(sys.argv[1:], "a:d:e:h:r:s:t:v:c:i:D:p:T:P:", ["affinity=", "directory=", "end=", "help", "retry=", "start=", "title=", "verbose=", "command=", "cmd=", "dependencies=", "priority=", "timeout=","parent=","localprogress=","globalprogress="])
-	if len(args) != 2 :
-		usage()
-		sys.exit(2)
-	serverUrl = args[0]
-        while serverUrl[-1] == '/':
-            serverUrl = serverUrl[:-1]
-	action = args[1]
-except getopt.GetoptError, err:
-	# print help information and exit:
-	print str(err) # will print something like "option -a not recognized"
-	usage()
-	sys.exit(2)
-for o, a in opts:
-	if o in ("-h", "--help"):
-		usage()
-		sys.exit(2)
-	elif o in ("-d", "--directory"):
-		dir = a
-	elif o in ("-s", "--start"):
-		startIndex = int(a)
-	elif o in ("-e", "--end"):
-		endIndex = int(a)
-	elif o in ("-v", "--verbose"):
-		verbose = True
-	elif o in ("-r", "--retry"):
-		retry = int(a)
-	elif o in ("-p", "--priority"):
-		priority = int(a)
-	elif o in ("-a", "--affinity"):
-		affinity = a
-	elif o in ("-t", "--title"):
-		title = a
-	elif o in ("-c", "--command", "--cmd"):
-		cmd=a
-	elif o in ("-i", "--jobid"):
-		id=a
-	elif o in ("-D", "--dependencies"):
-		dependencies=a
-	elif o in ("-T", "--timeout"):
-		timeout=int(a)
-	elif o in ("-P", "--parent"):
-		parent=int(a)
-	elif o in ("--localprogress"):
-		localprogress = a
-	elif o in ("--globalprogress"):
-		globalprogress = a
-	else:
-		assert False, "unhandled option " + o
+    def add(self, command, **kwargs):
+        '''
+        Submit a new job to the coalition server at server_url.
 
-# Log function
-def output (str):
-	if verbose:
-		print (str)
+        For example, assuming a coalition server is running at
+        http://localhost:19211, the following will submit a job to run the 'date'
+        command:
 
-if action=="add":
-	params = urllib.urlencode({'parent':parent, 'title':title, 'cmd':cmd, 'dir':dir, 'priority':priority, 'retry':retry, 'timeout':timeout, 'affinity':affinity, 'dependencies':dependencies, 'localprogress':localprogress, 'globalprogress':globalprogress})
-	conn = httplib.HTTPConnection(re.sub("^http://", "", serverUrl))
-	conn.request("GET", "/json/addjob?"+params)
-	response = conn.getresponse()
-	data = response.read()
-	conn.close()
-	print data
-	
-elif action=="list":
-	params = urllib.urlencode({'id':parent})
-	conn = httplib.HTTPConnection(re.sub("^http://", "", serverUrl))
-	conn.request("GET", "/json/getjobs?"+params)
-	response = conn.getresponse()
-	data = response.read()
-	conn.close()
+        >>> job_id = add('http://localhost:19211/', 'date', priority=2000, title='Date test')
+        >>> job_id is None
+        False
+        '''
+        options = deepcopy(self.default_options)
+        options.update(kwargs)
+        options.update({'cmd': command})
+        params = urllib.urlencode(options)
+        conn = httplib.HTTPConnection(self.server_url)
+        conn.request('GET', '/json/addjob?' + params)
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
+        return data
 
-	data = eval (data)
-	vars=data["Vars"]
-	print (vars)
-	jobs=data["Jobs"]
-	parents=data["Parents"]
-	
-	parents_info=''
-	for i in range(len(parents)):
-		parents_info = parents_info+ str(parents[i]["ID"])+" " +str(parents[i]["Title"])+ " > "
-	print(parents_info)
-	for i in range(len(jobs)):
-		for j in range(len(vars)):
-			print (jobs[i])
-elif action=="remove":
-	if id<0: 
-		print("Use option -i to specify the job id to remove")
-	else:
-		params = urllib.urlencode({'id':id})
-		conn = httplib.HTTPConnection(re.sub("^http://", "", serverUrl))
-		conn.request("GET", "/json/clearjobs?"+params)
-		response = conn.getresponse()
-		data = response.read()
-		conn.close()
-else:
-	print("I don't know what to do with myself. Use another action")
+    def list(self):
+        params = urllib.urlencode({'id':self.default_options['parent']})
+        conn = httplib.HTTPConnection(self.server_url)
+        conn.request("GET", "/json/getjobs?"+params)
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
+
+        data = re.sub(r',null', r',None', data)
+
+        data = eval(data)
+        vars = data["Vars"]
+        jobs = data["Jobs"]
+        parents = data["Parents"]
+        
+        parents_info=''
+        for i in range(len(parents)):
+            parents_info = parents_info+ str(parents[i]["ID"])+" " +str(parents[i]["Title"])+ " > "
+        print(parents_info)
+        for i in range(len(jobs)):
+            print jobs[i]
+
+    def remove(self, id):
+        if id < 0: 
+            print("Use option -i to specify the job id to remove")
+        else:
+            params = urllib.urlencode({'id':id})
+            conn = httplib.HTTPConnection(self.server_url)
+            conn.request("GET", "/json/clearjobs?" + params)
+            response = conn.getresponse()
+            data = response.read()
+            conn.close()
+
+
+
+def _parse_args(argv):
+    """Parses arguments, returns ``(options, args)``."""
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(description="""\
+Control the Coalition server located at SERVER_URL.""",
+                            epilog="""\
+(C) 2012  Christian Fobel, licensed under the terms of GPL3.""",)
+    parser.add_argument('-d', '--directory', dest='dir',
+            help='Working directory (default: %(default)s)')
+    parser.add_argument('-t', '--title',
+            help='Set the job title (default: %(default)s)')
+    parser.add_argument('-p', '--priority',
+            help='Priority of the job (default: %(default)s)')
+    parser.add_argument('-r', '--retry',
+            help='Number of retry this jobs can do (default: %(default)s)')
+    parser.add_argument('-a', '--affinity',
+            help='Affinity words to workers, separated by a comma '\
+                    '(default: %(default)s)')
+    parser.add_argument('-T', '--timeout',
+            help='timeout for the job')
+    parser.add_argument('-D', '--dependencies',
+            help='IDs of the dependent jobs (exemple : "21 22 23")')
+    parser.add_argument('--globalprogress', help='The job progression pattern')
+    parser.add_argument('--localprogress',
+            help='The second job progression pattern')
+    parser.add_argument(nargs=1,
+            dest='server_url', default='http://localhost:19211',
+            help='Server URL (default: %(default)s)')
+
+    subparsers = parser.add_subparsers(help='Action to perform')
+    list_parser = subparsers.add_parser('list',
+            help='list the jobs on the server')
+    list_parser.set_defaults(action='list')
+    add_parser = subparsers.add_parser('add',
+            help='add a job, use option -c for command')
+    add_parser.add_argument('-c', '--cmd', help='Command to add', required=True)
+    add_parser.set_defaults(action='add')
+    remove_parser = subparsers.add_parser('remove',
+            help='remove job designated by id') 
+    remove_parser.add_argument('-i', '--jobid', dest='id', required=True,
+            help='ID of the Job')
+    remove_parser.set_defaults(action='remove')
+    parser.set_defaults(**CoalitionControl.default_options)
+    args = parser.parse_args(argv)
+
+    args.server_url = args.server_url[0]
+
+    return args
+
+
+if __name__ == '__main__':
+    args = _parse_args(sys.argv[1:])
+    control = CoalitionControl(args.server_url)
+    print args
+
+    if args.action == 'list':
+        control.list()
+    elif args.action == 'remove':
+        control.remove(args.id)
+    elif args.action == 'add':
+        # not included: args.id
+        # startIndex=args.startIndex, endIndex=args.endIndex,
+        control.add(args.cmd, dir=args.dir, title=args.title,
+            retry=args.retry, affinity=args.affinity, priority=args.priority,
+            timeout=args.timeout, parent=args.parent,
+            dependencies=args.dependencies, localprogress=args.localprogress,
+            globalprogress=args.globalprogress)
